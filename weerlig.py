bf24a192f7c63b57e34bdb37409f6092aa20e181
Maik Hoepfel <m@maikhoepfel.de>
2017-02-11 14:03:30 +0100

    button foo

--- weerlig.py
+++ weerlig.py
@@ -2,18 +2,24 @@
 import apa102
 import time
 import RPi.GPIO as GPIO
+import gpiozero
 
 total_leds = 60
 brightness = 31  # 0..31
 lightning_talk_time = 30 # in seconds
 rounds_per_minute = 40
 
-SWITCH_GPIO = 7  # board numbering, equals GPIO04
+SWITCH_GPIO = 4  # BCM numbering, equals board pin 7
 
 
 def init_strip():
     # Initialize the class
     return apa102.APA102(numLEDs=total_leds, globalBrightness=brightness)
+
+
+def init_buttons():
+    # Let pins confirm to board numbering
+    return gpiozero.Button(SWITCH_GPIO, bounce_time=0.1)
 
 
 def show_idle_strip(strip):
@@ -44,7 +50,7 @@ def set_and_show_strip(strip, progress):
     strip.show()
 
 
-def visualize(strip):
+def visualize(strip, button):
     start_of_talk = current_time = time.time()
     end_of_talk = start_of_talk + lightning_talk_time
     while current_time < end_of_talk:
@@ -52,21 +58,29 @@ def visualize(strip):
         progress = elapsed_seconds / lightning_talk_time
         set_and_show_strip(strip, progress)
         current_time = time.time()
-        print(GPIO.input(SWITCH_GPIO))
-    print('Finished')
+        if button.is_pressed:
+            print("Aborted because of button press")
+            return
+
 
 if __name__ == '__main__':
+    # Initialize
     strip = init_strip()
-    # Let pins confirm to board numbering
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(SWITCH_GPIO, GPIO.IN)
+    button = init_buttons()
 
-    show_idle_strip(strip)
-    try:
-        visualize(strip)
-    except KeyboardInterrupt:
-        print('User aborted')
-    else:
+    while True:
+        # Power up strip in initial state
         show_idle_strip(strip)
-    finally:
-        strip.cleanup()
+
+        print("Waiting for button press")
+        button.wait_for_press()
+        button.wait_for_release()
+
+        print("Starting")
+        # Aborts early if button is pressed
+        visualize(strip, button)
+
+        print("Finished")
+
+
+        #TODO strip.cleanup()
